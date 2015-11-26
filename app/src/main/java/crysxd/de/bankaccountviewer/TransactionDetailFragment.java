26f959b5e545e805632854eb269fef75ac6b36ab
Christian Würthner <c.wuerthner@me.com>
2015-11-26 16:49:23 +0100

    Added items to detail list

--- app/src/main/java/crysxd/de/bankaccountviewer/TransactionDetailFragment.java
+++ app/src/main/java/crysxd/de/bankaccountviewer/TransactionDetailFragment.java
@@ -1,19 +1,22 @@
 package crysxd.de.bankaccountviewer;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
-import crysxd.de.bankaccountviewer.dummy.DummyContent;
 import me.figo.models.Transaction;
 
 /**
@@ -33,6 +36,8 @@ public class TransactionDetailFragment extends Fragment implements LoadTaskCallb
     private String mAccountId;
     private String mAccountName;
     private ProgressDialog mProgressDialog;
+    private RecyclerView mRecyclerView;
+    private View mRoot;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -50,6 +55,8 @@ public class TransactionDetailFragment extends Fragment implements LoadTaskCallb
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
             mAccountId = mAccountName = getArguments().getString(ARG_ITEM_ID);
+            TransactionLoadTask task = new TransactionLoadTask(this, this.mAccountId);
+            task.execute();
 
         }
 
@@ -62,12 +69,15 @@ public class TransactionDetailFragment extends Fragment implements LoadTaskCallb
         if (appBarLayout != null) {
             appBarLayout.setTitle(mAccountName);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.transaction_detail, container, false);
+        mRoot =  inflater.inflate(R.layout.transaction_detail, container, false);
+        mRecyclerView = (RecyclerView) mRoot.findViewById(R.id.recyclerView);
+        return mRoot;
     }
 
     @Override
@@ -104,5 +114,61 @@ public class TransactionDetailFragment extends Fragment implements LoadTaskCallb
     @Override
     public void onResult(List<Transaction> result) {
 
+        FrameLayout.LayoutParams pView = (FrameLayout.LayoutParams) mRecyclerView.getLayoutParams();
+        pView.height = mRoot.getHeight();
+        mRecyclerView.setLayoutParams(pView);
+        Log.e(this.getClass().getSimpleName(), "Height: " + pView.height);
+
+        assert mRecyclerView != null;
+        mRecyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(result));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<Transaction> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<Transaction> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_2, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mTitleView.setText(mValues.get(position).getName());
+            holder.mDetailView.setText(mValues.get(position).getAmount() + mValues.get(position).getCurrency());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mTitleView;
+            public final TextView mDetailView;
+            public Transaction mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mTitleView = (TextView) view.findViewById(android.R.id.text1);
+                mDetailView = (TextView) view.findViewById(android.R.id.text2);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mDetailView.getText() + "'";
+            }
+        }
     }
 }
